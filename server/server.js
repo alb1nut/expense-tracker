6e90f2829c40c64e1b465fc9ead32c25b30374ea
Albert Nutifafa Dovlo <albert.dovlo@amalitech.org>
2023-01-01 11:42:24 +0000

    first commit

--- server/server.js
+++ server/server.js
@@ -1,20 +1,25 @@
 import express from "express";
-import mongoose from "mongoose"
+import mongoose from "mongoose";
+import cors from "cors";
 
+const PORT = 4000; 
+const app = express();
 
-const PORT =5000
-const app =express();
+app.use(cors())
 
-mongoose.connect(
+mongoose
+  .connect(
     "mongodb+srv://al_dov:nutifafa123456789@cluster0.zwtqyyu.mongodb.net/?retryWrites=true&w=majority"
-    ).then(()=> console.log("MongoDB connection successful")).catch(()=>{
-        console.log("Error");
-    });
+  )
+  .then(() => console.log("MongoDB connection successful"))
+  .catch(() => {
+    console.log("Error");
+  });
 
-app.get('/' , (req,res)=>{ 
-    res.send('Hello')
-})
+app.get("/", (req, res) => {
+  res.send("Hello");
+});
 
-app.listen(PORT, ()=>{
-    console.log(`I am listening at PORT ${PORT}`);
-})
+app.listen(PORT, () => {
+  console.log(`I am listening at PORT ${PORT}`);
+});
